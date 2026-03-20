b151d38e1595f3634673a0bdbdcb866b1823dbf8
Romuald Wandji <romualdesdras.wandji@kindredgroup.com>
2026-03-20 11:53:48 +0100

    Revert "Merge branch 'feat/msgraph-integration' into test/librechat-0.8.3"
    This reverts commit 96dd93657f8e6664adf1ab76dad3fd23b8501703, reversing
    changes made to 9c8aa57622b14bc098446a9b4ab50ccdd2f408ed.

--- api/server/services/MCP.js
+++ api/server/services/MCP.js
@@ -35,7 +35,6 @@ const db = require('~/models');
 const { findToken, createToken, updateToken, deleteTokens } = db;
 const { getGraphApiToken } = require('./GraphTokenService');
 const { reinitMCPServer } = require('./Tools/mcp');
-const { getGraphApiToken } = require('./GraphTokenService');
 const { getAppConfig } = require('./Config');
 const { getLogStores } = require('~/cache');
 
@@ -729,7 +728,6 @@ function createToolInstance({
         user: config?.configurable?.user,
         requestBody: config?.configurable?.requestBody,
         customUserVars,
-        graphTokenResolver: getGraphApiToken,
         flowManager,
         tokenMethods: {
           findToken,
